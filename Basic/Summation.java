029347d1565380d0c20d580f64095e6f2506a0fb
Sankha Subhra Mondal <sankhamondal400@gmail.com>
2022-02-12 02:20:51 +0530

    Summation of two numbers

--- Basic/Summation.java
+++ Basic/Summation.java
@@ -1,10 +1,10 @@
-/*  Subtraction of two numbers using java  */
+/*  Summation of two numbers using java  */
 
-class Subtraction {
+class Summation {
 	public static void main(String[] args) {
 		int x = 50;
 		int y = 20;
-		int z = x-y;
+		int z = x+y;
 		System.out.println(z);
 	}
 }
