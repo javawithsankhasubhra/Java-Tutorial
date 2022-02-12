95d0c93f1a2355b5f8b4765faffce7bbd20666c9
Sankha Subhra Mondal <sankhamondal400@gmail.com>
2022-02-12 17:15:02 +0530

    Prime Number Calculation

--- Basic/PrimeNumber.java
+++ Basic/PrimeNumber.java
@@ -1,22 +1,25 @@
 /*  Write a program to print a number is Prime or Not  */
-
+   
 import java.util.Scanner;
 
-public class PrimeNumber {     
-public static void main(String[] args){        
-       
-        Scanner sc = new Scanner(System.in);   
-        int number = sc.nextInt(),count=0;        
-        for(int i=2 ; i<number ; i++) {            
-            if(number % i==0) {                
-               	count++;            
-                }        
-            }  
-                  
-        if(count==0)            
-            System.out.println(number + " is Prime");           
-        else           
-            System.out.println(number + " is Not Prime");   
-                                    
-       }
+class PrimeNumber {    
+     public static void main(String args[]) {
+     Scanner sc = new Scanner(System.in);
+     int flag=0;      
+     int n = sc.nextInt();  
+
+  if(n==0 || n==1) 
+        System.out.println(n+" is not prime number");      
+  else {  
+        for(int i=2;i<=n/2;i++){      
+        if(n%i==0){      
+            System.out.println(n+" is not prime number");      
+            flag=1;      
+            break;      
+        }      
+   }      
+   if(flag==0)   
+        System.out.println(n+" is prime number"); }  
+   
+    }    
 }
